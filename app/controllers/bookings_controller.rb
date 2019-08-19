78d57f51f9fe8657863fc7fbba596631dcaffdc5
Charles Dieulefet <charles.dieulefet@gmail.com>
2019-08-19 18:12:39 +0200

    Bookings controller done

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -5,15 +5,19 @@ class BookingsController < ApplicationController
   end
 
   def show
+    @user = User.find(params[:id])
+    @trip = Trip.find(params[:trip_id])
     @booking = Booking.find(params[:booking_id])
   end
 
   def new
+    @user = User.find(params[:id])
     @trip = Trip.find(params[:trip_id])
     @booking = Booking.new
   end
 
   def create
+    @user = User.find(params[:id])
     @trip = Trip.find(params[:trip_id])
     @booking = Booking.new(booking_params)
     @booking.trip = @trip
@@ -25,18 +29,32 @@ class BookingsController < ApplicationController
   end
 
   def edit
-
+    @user = User.find(params[:id])
+    @trip = Trip.find(params[:trip_id])
+    @booking = Booking.find(params[:booking_id])
   end
 
   def update
+    @user = User.find(params[:id])
+    @trip = Trip.find(params[:trip_id])
+    @booking = Booking.find(params[:booking_id])
+      if @booking.update(booking_params)
+        redirect_to trip_booking_path(@booking)
+      else render :edit
+      end
   end
 
   def destroy
+    @user = User.find(params[:id])
+    @trip = Trip.find(params[:trip_id])
+    @booking = Booking.find(params[:booking_id])
+    @booking.destroy
+    redirect_to trip_bookings_path(@bookings)
   end
 
   private
 
   def booking_params
-    params.require(:booking).permit(:start_date, :end_date, :user_id, :review_id, :trip_id)
+    params.require(:booking).permit(:start_date, :end_date, :user_id, :trip_id)
   end
 end
