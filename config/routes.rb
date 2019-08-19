3dc64f62e1c3e59b12c732f937373d18e58725cb
Charles Dieulefet <charles.dieulefet@gmail.com>
2019-08-19 16:07:38 +0200

    Routes done

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'trips#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-
+  resources :trips do
+    resources :bookings
+  end
 end
