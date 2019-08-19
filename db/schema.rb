bd60e1eb6a14aa4d4c4489c4da7aa4ea6b2fa961
Maxime Fulconis <maximefulconis@gmail.com>
2019-08-19 17:42:09 +0200

    migration add description to trips

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_08_19_133353) do
+ActiveRecord::Schema.define(version: 2019_08_19_152954) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -32,6 +32,7 @@ ActiveRecord::Schema.define(version: 2019_08_19_133353) do
     t.bigint "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "description"
     t.index ["user_id"], name: "index_trips_on_user_id"
   end
 
