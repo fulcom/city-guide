8415f636fb159335689b2dd31b5bbae4ccd34600
Charles Dieulefet <charles.dieulefet@gmail.com>
2019-08-21 12:06:05 +0200

    Datepicker ok

---
+++ app/javascript/plugins/flatpickr.js
@@ -0,0 +1,8 @@
+import flatpickr from "flatpickr"
+import "flatpickr/dist/flatpickr.min.css" // Note this is important!
+import "flatpickr/dist/themes/airbnb.css" // A path to the theme CSS
+
+
+flatpickr(".datepicker", {allowInput: true})
+
+// plugins: [new rangePlugin({ input: "#range_end"})]
