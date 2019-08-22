6c09358938090d3b88b4905b9c8f26ea6ef91329
Alice Fabre <alice.fabre@hotmail.fr>
2019-08-22 14:20:45 +0200

    implémentation de la map dans vue index trip

---
+++ app/javascript/plugins/init_mapbox.js
@@ -0,0 +1,58 @@
+import mapboxgl from 'mapbox-gl';
+
+const initMapbox = () =>
+{
+  const mapElement = document.getElementById('map');
+  // document.getElementById('map') : va chercher dans l'html index trip la balise où id = map
+
+  const fitMapToMarkers = (map, markers) =>
+  {
+  const bounds = new mapboxgl.LngLatBounds(); // bound : définir les frontières de ma map
+  markers.forEach(marker => bounds.extend([ marker.lng, marker.lat ]));
+  map.fitBounds(bounds, { padding: 70, maxZoom: 15, duration: 0 });
+  };
+
+  const addMarkersToMap = (map, markers) =>
+  {
+    markers.forEach((marker) =>
+    {
+      const popup = new mapboxgl.Popup().setHTML(marker.infoWindow); // add this
+
+      new mapboxgl.Marker()
+        .setLngLat([ marker.lng, marker.lat ])
+        .setPopup(popup) // add this
+        .addTo(map);
+    });
+  };
+
+  if (mapElement)
+  { // only build a map if there's a div#map to inject into
+    mapboxgl.accessToken = mapElement.dataset.mapboxApiKey;
+    // mapElement défini plus haut (renvoie depuis l'html index trip la balise où id = map)
+    // .dataset.mapboxApiKey : dans cette balise, va chercher la data(pour dataset) de mapbox-api-key (pour mapboxApiKey)
+
+    const map = new mapboxgl.Map(
+    {
+      container: 'map', // id de la div qui va contenir la map
+      style: 'mapbox://styles/mapbox/streets-v10' // sur le site de mapbox
+    });
+
+    const markers = JSON.parse(mapElement.dataset.markers);
+    // console.log(markers)
+
+    // markers.forEach((marker) =>
+    // {
+    //   new mapboxgl.Marker()
+    //     .setLngLat([ marker.lng, marker.lat ])
+    //     .addTo(map);
+    // });
+
+    fitMapToMarkers(map, markers);
+
+    addMarkersToMap(map, markers);
+  }
+};
+
+
+export { initMapbox };
+// export { addMarkersToMap};
