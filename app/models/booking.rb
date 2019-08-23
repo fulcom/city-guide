a9f8388d4e2c42cb922e0430cba57874eb89b263
Alice Fabre <alice.fabre@hotmail.fr>
2019-08-23 12:52:15 +0200

    codage de accepter ou refuser un booking

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -1,4 +1,10 @@
 class Booking < ApplicationRecord
   belongs_to :user
   belongs_to :trip
+
+  STATUSES = {
+    "pending" => "En attente de confirmation",
+    "accepted" => "Réservation confirmée",
+    "declined" => "Demande refusée"
+  }
 end
