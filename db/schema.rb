4be8b4087a1a7b92c405decd029a919a7364e239
Alice Fabre <alice.fabre@hotmail.fr>
2019-08-23 12:41:49 +0200

    j'avais rentré en dur la valeur pending en temps que valeur par défaut de status. Je l'ai enlevé et généré une migration à la place

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_08_22_165305) do
+ActiveRecord::Schema.define(version: 2019_08_23_100823) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -22,7 +22,7 @@ ActiveRecord::Schema.define(version: 2019_08_22_165305) do
     t.bigint "trip_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.string "status"
+    t.string "status", default: "pending"
     t.index ["trip_id"], name: "index_bookings_on_trip_id"
     t.index ["user_id"], name: "index_bookings_on_user_id"
   end
